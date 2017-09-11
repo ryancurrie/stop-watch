0b20b422f18f98070ef378c0c36e1893fe32a45d
Ryan Currie <ryan.currie@gmail.com>
2017-09-11 13:29:54 -0700

    Completed feature requirements.

--- main.js
+++ main.js
@@ -1,38 +1,43 @@
 var tick = 0
 var watch1
 var watch2
-var limit
+var limit = null
 var seconds = document.querySelector('#seconds')
 var startButton = document.querySelector('#start')
 var stopButton = document.querySelector('#stop')
 var resetButton = document.querySelector('#reset')
 var setButton = document.querySelector('#set')
 var userLimit = document.querySelector('#limit')
+var background = ['#00E6F0', '#00FF00', '#FFFF00', '#FF6800', '#FF14AB']
 
 var start = function() {
   watch1 = setInterval(function () {
-    if (tick <= limit) {
-      tick++
-    }
-    else {
-      stop()
+      if (limit === null || tick < limit) {
+        tick++
+        document.querySelector('#start').disabled = true;}
+      else {
+        stop()
+      }
   }, 1000)
 }
 
 var update = function () {
   watch2 = setInterval(function () {
     seconds.textContent = tick
+    document.querySelector('.container').style.background = background[Math.floor(Math.random() * background.length) + 1  ]
   }, 1000)
-}
-
-var stop = function() {
-  clearInterval(watch1)
-  clearInterval(watch2)
 }
 
 var reset = function() {
   tick = 0
   seconds.textContent = tick
+  document.querySelector('.container').style.background = '#fff'
+}
+
+var stop = function() {
+  clearInterval(watch1)
+  clearInterval(watch2)
+  document.querySelector('#start').disabled = false;
 }
 
 var setLimit = function() {
