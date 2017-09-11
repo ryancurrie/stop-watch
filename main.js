4df79ff251156a7e516b49f94e028163bd1df076
Ryan Currie <ryan.currie@gmail.com>
2017-09-11 10:01:33 -0700

    Completed issue 5 #5.

--- main.js
+++ main.js
@@ -1,19 +1,36 @@
 var tick = 0
-
+var watch1
+var watch2
 var seconds = document.querySelector('#seconds')
 var startButton = document.querySelector('#start')
+var stopButton = document.querySelector('#stop')
+var resetButton = document.querySelector('#reset')
 
-function start() {
-  setInterval(function () {
+var start = function() {
+  watch1 = setInterval(function () {
     tick++
   }, 1000)
 }
 
-function update() {
-  setInterval(function () {
+var update = function () {
+  watch2 = setInterval(function () {
     seconds.textContent = tick
-  })
+  }, 1000)
+}
+
+var stop = function() {
+  clearInterval(watch1)
+  clearInterval(watch2)
+}
+
+var reset = function() {
+  tick = 0
+  seconds.textContent = tick
 }
 
 startButton.addEventListener('click', start)
 startButton.addEventListener('click', update)
+
+stopButton.addEventListener('click', stop)
+
+resetButton.addEventListener('click', reset)
